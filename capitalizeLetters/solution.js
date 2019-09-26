d9811538f55e7cb8542f4f6868d7025ef82d1ce6
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-09-26 12:53:19 +0100

    Added a regex approach using named capture group

--- capitalizeLetters/solution.js
+++ capitalizeLetters/solution.js
@@ -3,6 +3,16 @@ function capitalizeFirstLetter([first, ...others]) {
   return str.replace(/,/gi, "");
 }
 
+function firstToUpperCase(letter){
+  return letter.toUpperCase()
+}
+
+function regexCapitalizeFirstLetter(str){
+  return str.replace(/^(?<first>\w)/g, firstToUpperCase)
+}
+
 const arr = ["h", "e", "l", "p"];
+
 console.log(capitalizeFirstLetter("help"));
+console.log(regexCapitalizeFirstLetter('help'));
 console.log(capitalizeFirstLetter(arr));
